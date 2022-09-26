b1233c7859f1250fc77a2e2e0044ebafe17e3e48
roma <roma@unictive.com>
2022-09-27 05:35:22 +0700

    update membuat repocitory

--- main.go
+++ main.go
@@ -3,48 +3,12 @@ package main
 import (
 	"bwastartup/user"
 	"log"
-	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// dsn := "root:@tcp(127.0.0.1:3306)/donasi?charset=utf8mb4&parseTime=True&loc=Local"
-	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-
-	// fmt.Println("koneksi database berhasil")
-
-	// var users []user.User
-	// ini get awal masih kosong
-	// length := len(users)
-	// fmt.Println(length)
-
-	// fmt.Println("====================")
-
-	// db.Find(&users)
-	// setelah di get dr db
-	// length = len(users)
-	// fmt.Println(length)
-
-	// for _, user := range users {
-	// 	fmt.Println(user.ID)
-	// 	fmt.Println(user.Name)
-	// 	fmt.Println(user.Email)
-	// }
-
-	router := gin.Default()
-	router.GET("/users", handler)
-	router.Run()
-
-}
-
-func handler(c *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/donasi?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -52,9 +16,24 @@ func handler(c *gin.Context) {
 		log.Fatal(err.Error())
 	}
 
-	var users []user.User
-	db.Find(&users)
+	userRepocitory := user.NewRepocitory(db)
 
-	c.JSON(http.StatusOK, users)
+	// tes
+	user := user.User{
+		Name:       "Test Simpan aja",
+		Occupation: "IT Analyst",
+		Email:      "roma@gmail.com",
+		Password:   "password",
+		Avatar:     "usera.jpg",
+		Role:       "user",
+	}
+
+	userRepocitory.Save(user)
 
 }
+
+// proses nya
+// input = user melakukan input
+// handler = menangkap inputan dr user, di map ke sebuah object atau struct, buat struct baru yg di input sama user
+// service = mengambil object mapping yg udah di buat d handler lalu di masukan k struct Induk yg ada di entity
+//repository = save data ke db, ini untuk interaksi k db
